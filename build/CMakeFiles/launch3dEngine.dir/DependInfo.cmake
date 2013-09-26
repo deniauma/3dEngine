7a8fe107cba764f3eb1b50012120cda500735d7e
Deniaud <mathieu.deniaud@gmail.com>
2013-09-26 12:07:00 +0200

    Import project

---
+++ build/CMakeFiles/launch3dEngine.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/mdeniaud/Projects/3dEngine/src/main.cpp" "/home/mdeniaud/Projects/3dEngine/build/CMakeFiles/launch3dEngine.dir/src/main.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/mdeniaud/Projects/3dEngine/build/external/glfw-3.0.3/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../external/glfw-3.0.3/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ build/CMakeFiles/launch3dEngine.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/launch3dEngine.dir/src/main.cpp.o"
+  "launch3dEngine.pdb"
+  "launch3dEngine"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/launch3dEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
